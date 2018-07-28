ed6327f4311b26417d1837e12f5a0f41366eedea
Amal Wickramarathna <wickramarathna.amal@gmail.com>
2018-07-29 00:23:48 +0530

    add cart reference dao interface

---
+++ WN-2FromWN-1/src/java/ifix/dao/CartReferencesDao.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifix.dao;
+
+import ifix.model.CartReferences;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Amal
+ */
+public interface CartReferencesDao {
+
+    boolean addReference(CartReferences cartReferences) throws SQLException;
+
+    ResultSet getAllCartReferences() throws SQLException;
+
+    boolean updateCartReferences(CartReferences cartReferences) throws SQLException;
+
+    ResultSet getCartReferencesByAttribute(String attribute, String condition, String value) throws SQLException;
+
+}
