a74f4230cf0c3553f1a485564aa28e92d8c5059c
Amal Wickramarathna <wickramarathna.amal@gmail.com>
2018-08-03 00:16:50 +0530

    add new features of laptops
    update imageuploadDaoImpl with newly added fields

--- WN-2FromWN-1/src/java/ifix/dao/Impl/imageUploadDaoImpl.java
+++ WN-2FromWN-1/src/java/ifix/dao/Impl/imageUploadDaoImpl.java
@@ -19,18 +19,33 @@ import java.sql.SQLException;
  */
 public class imageUploadDaoImpl implements imageUploadDao {
 
-    private String selectQuery = "select imageUpload_id, imageUpload_path, imageUpload_file_name, "
-            + " imageupload_item_desc, imageupload_price from imageupload";
+    private String selectQuery = "select imageUpload_id, imageUpload_path, "
+            + " imageUpload_file_name, imageupload_item_desc, imageupload_price, "
+            + " imageupload_model, imageupload_processor, imageupload_os, "
+            + " imageupload_graphics, imageupload_webcam, imageupload_memory, "
+            + " imageupload_storage, imageupload_display from imageupload";
 
     @Override
     public boolean addItem(ImageUpload imageUpload) throws SQLException {
         Connection con = DatabaseConnection2.getDatabaseConnection();
-        PreparedStatement ps = con.prepareStatement("insert into imageupload(imageUpload_path,"
-                + " imageUpload_file_name, imageupload_item_desc, imageupload_price) values (?,?,?,?); ");
+        PreparedStatement ps = con.prepareStatement("insert into imageupload(imageUpload_id, "
+                + " imageUpload_path, imageUpload_file_name, imageupload_item_desc, "
+                + " imageupload_price, imageupload_model, imageupload_processor, "
+                + " imageupload_os, imageupload_graphics, imageupload_webcam, "
+                + " imageupload_memory, imageupload_storage, imageupload_display) "
+                + " values (?,?,?,?,?,?,?,?,?,?,?,?,?); ");
         ps.setString(1, imageUpload.getImgPath());
         ps.setString(2, imageUpload.getFileName());
         ps.setString(3, imageUpload.getItemDescription());
         ps.setBigDecimal(4, imageUpload.getPrice());
+        ps.setString(5, imageUpload.getModel());
+        ps.setString(6, imageUpload.getProcesser());
+        ps.setString(7, imageUpload.getOs());
+        ps.setString(8, imageUpload.getGraphics());
+        ps.setString(9, imageUpload.getWebcam());
+        ps.setString(10, imageUpload.getMemory());
+        ps.setString(11, imageUpload.getStorage());
+        ps.setString(12, imageUpload.getDisplay());
         ps.executeUpdate();
         ps.close();
         return true;
@@ -38,7 +53,29 @@ public class imageUploadDaoImpl implements imageUploadDao {
 
     @Override
     public boolean updateItem(ImageUpload imageUpload) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection con = DatabaseConnection2.getDatabaseConnection();
+        PreparedStatement ps = con.prepareStatement("update imageupload set imageUpload_path=?, "
+                + " imageUpload_file_name=?, imageupload_item_desc=?, "
+                + " imageupload_price=?, imageupload_model=?, imageupload_processor=?, "
+                + " imageupload_os=?, imageupload_graphics=?, imageupload_webcam=?, "
+                + " imageupload_memory=?, imageupload_storage=?, imageupload_display=? where imageUpload_id=?");
+        ps.setString(1, imageUpload.getImgPath());
+        ps.setString(2, imageUpload.getFileName());
+        ps.setString(3, imageUpload.getItemDescription());
+        ps.setBigDecimal(4, imageUpload.getPrice());
+        ps.setString(5, imageUpload.getModel());
+        ps.setString(6, imageUpload.getProcesser());
+        ps.setString(7, imageUpload.getOs());
+        ps.setString(8, imageUpload.getGraphics());
+        ps.setString(9, imageUpload.getWebcam());
+        ps.setString(10, imageUpload.getMemory());
+        ps.setString(11, imageUpload.getStorage());
+        ps.setString(12, imageUpload.getDisplay());
+        ps.setInt(13, imageUpload.getId());
+        ps.executeUpdate();
+        ps.close();
+        return true;
+
     }
 
     @Override
