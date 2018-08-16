d9845dbb475d0204dfba1d57337ee7f6b26116fa
Amal Wickramarathna <wickramarathna.amal@gmail.com>
2018-08-17 04:11:27 +0530

    fix bug [ ref #3  ]

--- WN-2FromWN-1/src/java/ifix/dao/Impl/invoiceDaoImpl.java
+++ WN-2FromWN-1/src/java/ifix/dao/Impl/invoiceDaoImpl.java
@@ -27,22 +27,21 @@ public class invoiceDaoImpl implements invoiceDao {
     @Override
     public MethodStatus addInvoice(Invoice invoice) throws SQLException {
         Connection con = DatabaseConnection2.getDatabaseConnection();
-        PreparedStatement ps = con.prepareStatement("INSERT INTO invoice(invoice_id, invoice_date, invoice_time, invoice_user_id, "
+        PreparedStatement ps = con.prepareStatement("INSERT INTO invoice(invoice_date, invoice_time, invoice_user_id, "
                 + " invoice_status, invoice_delever_status, invoice_total, invoice_address, invoice_city, invoice_country, "
                 + " invoice_items_qty, invoice_name) "
-                + "VALUES(?,?,?,?,?,?,?,?,?,?,?,?)");
-        ps.setInt(1, invoice.getInvoiceId());
-        ps.setDate(2, invoice.getInvoiceDate());
-        ps.setTime(3, invoice.getInvoiceTime());
-        ps.setInt(4, invoice.getInvoiceUserId());
-        ps.setInt(5, invoice.getInvoiceStatus());
-        ps.setInt(6, invoice.getInvoiceDeleverStatus());
-        ps.setBigDecimal(7, invoice.getInvoiceTotal());
-        ps.setString(8, invoice.getAddress());
-        ps.setString(9, invoice.getCity());
-        ps.setString(10, invoice.getCountry());
-        ps.setInt(11, invoice.getQty());
-        ps.setString(12, invoice.getCustName());
+                + "VALUES(?,?,?,?,?,?,?,?,?,?,?)");
+        ps.setDate(1, invoice.getInvoiceDate());
+        ps.setTime(2, invoice.getInvoiceTime());
+        ps.setInt(3, invoice.getInvoiceUserId());
+        ps.setInt(4, invoice.getInvoiceStatus());
+        ps.setInt(5, invoice.getInvoiceDeleverStatus());
+        ps.setBigDecimal(6, invoice.getInvoiceTotal());
+        ps.setString(7, invoice.getAddress());
+        ps.setString(8, invoice.getCity());
+        ps.setString(9, invoice.getCountry());
+        ps.setInt(10, invoice.getQty());
+        ps.setString(11, invoice.getCustName());
         ps.executeUpdate();
         ps.close();
         return MethodStatus.SUCCESS;
